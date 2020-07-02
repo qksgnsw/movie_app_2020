06864e683edc47aabb241a656b1ff48e28eaa8a9
qksgnsw <qksgns0416@icloud.com>
2020-07-02 11:12:51 +0900

    finish

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,10 @@
 import React from "react"
+import './Detail.css'
 
 class Detail extends React.Component{
     componentDidMount() {
         const { location, history } = this.props
+        console.log(location)
         if (location.state === undefined) {
             history.push('/')
         }
@@ -11,7 +13,23 @@ class Detail extends React.Component{
     render() {
         const { location } = this.props
         if (location.state) {
-            return <span>{ location.state.title }</span>
+            return (
+                <div>
+                    <img src={ location.state.poster } alt={ location.state.title } />
+                    <div>
+                        <h3>
+                            { location.state.title }
+                        </h3>
+                        <h5>{ location.state.year }</h5>
+                        <ul>
+                            { location.state.genres.map((genre, index)=>{
+                            return <li key={ index }>{ genre }</li>
+                            })}
+                        </ul>
+                        <p> { location.state.summary }</p>
+                    </div>
+                </div>
+            )
         } else {
             return null
         }
